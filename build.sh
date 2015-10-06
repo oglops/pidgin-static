a082b0755382b9fd52076dc6fa050209121e089b
oglop <oglop@t450s.primary>
2015-10-06 17:56:25 +0800

    update pidgin to 2.10.11, assume you are installing it at home

--- build.sh
+++ build.sh
@@ -31,22 +31,28 @@ cd `dirname $0`
 ENV_ROOT=`pwd`
 . ./env.source
 
-rm -rf "$BUILD_DIR" "$TARGET_DIR"
-mkdir -p "$BUILD_DIR" "$TARGET_DIR"
+
+rm -rf "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"
+# rm -rf "$BUILD_DIR" "$TARGET_DIR"
+# mkdir -p "$BUILD_DIR" "$TARGET_DIR"
 
 # NOTE: this is a fetchurl parameter, nothing to do with the current script
 #export TARGET_DIR_DIR="$BUILD_DIR"
 
 echo "#### pidgin build, by oglops ####"
 cd $BUILD_DIR
-../fetchurl "http://downloads.sourceforge.net/project/pidgin/Pidgin/2.10.9/pidgin-2.10.9.tar.bz2"
+../fetchurl "http://downloads.sourceforge.net/project/pidgin/Pidgin/2.10.11/pidgin-2.10.11.tar.bz2"
 git clone https://github.com/xiehuc/pidgin-lwqq.git
 git clone https://github.com/xiehuc/lwqq.git
-git clone git@github.com:oglops/pidgin-sendscreenshot.git
-../fetchurl "http://dist.schmorp.de/libev/libev-4.15.tar.gz"
-# ../fetchurl "http://ftp.mozilla.org/pub/mozilla.org/js/mozjs-24.2.0.tar.bz2"
-../fetchurl "http://ftp.mozilla.org/pub/mozilla.org/js/mozjs17.0.0.tar.gz"
-# ../fetchurl "http://ftp.gnu.org/gnu/autoconf/autoconf-2.13.tar.gz"
+git clone https://github.com/oglops/pidgin-sendscreenshot.git
+
+../fetchurl "http://dist.schmorp.de/libev/libev-4.20.tar.gz"
+# # ../fetchurl "http://ftp.mozilla.org/pub/mozilla.org/js/mozjs-24.2.0.tar.bz2"
+# # ../fetchurl "http://ftp.mozilla.org/pub/mozilla.org/js/mozjs17.0.0.tar.gz"
+# # ../fetchurl "http://ftp.gnu.org/gnu/autoconf/autoconf-2.13.tar.gz"
+
+
 ../fetchurl "https://pidgin-gnome-keyring.googlecode.com/files/pidgin-gnome-keyring-1.20_src.tar.gz"
 git clone https://github.com/interskh/Recent-Contacts-Plugin-for-Pidgin.git
 git clone https://github.com/tony2001/pidgin-libnotify.git
@@ -58,103 +64,110 @@ git clone https://github.com/tony2001/pidgin-libnotify.git
 # make 
 # make install
 
-echo "*** Building libev ***"
-cd $BUILD_DIR/libev*
-# ./configure --enable-shared=no --prefix=$TARGET_DIR
-./configure --prefix=$TARGET_DIR
-make 
-make install
-libev_pc=$TARGET_DIR/lib/pkgconfig/libev.pc
-if [ ! -f $libev_pc ]; then
-  echo "#### create libev.pc ####"
-  mkdir -p $TARGET_DIR/lib/pkgconfig
-  cat <<EOF > $libev_pc
+# echo "*** Building libev ***"
+# cd $BUILD_DIR/libev*
+# # ./configure --enable-shared=no --prefix=$TARGET_DIR
+# ./configure --prefix=$TARGET_DIR
+# make 
+# make install
+# libev_pc=$TARGET_DIR/lib/pkgconfig/libev.pc
+# if [ ! -f $libev_pc ]; then
+#   echo "#### create libev.pc ####"
+#   mkdir -p $TARGET_DIR/lib/pkgconfig
+#   cat <<EOF > $libev_pc
 
-  prefix=$TARGET_DIR
-  exec_prefix=\${prefix}
-  libdir=\${prefix}/lib
-  includedir=\${exec_prefix}/include/libev
+#   prefix=$TARGET_DIR
+#   exec_prefix=\${prefix}
+#   libdir=\${prefix}/lib
+#   includedir=\${exec_prefix}/include/libev
 
-  Name: libev
-  Description: High-performance event loop/event model
-  Version: 4.03
-  Libs: -L\${libdir} -lev
-  Libs.private: 
-  Cflags: -I\${includedir}
-EOF
+#   Name: libev
+#   Description: High-performance event loop/event model
+#   Version: 4.03
+#   Libs: -L\${libdir} -lev
+#   Libs.private: 
+#   Cflags: -I\${includedir}
+# EOF
 
-fi
-mkdir -p $TARGET_DIR/include/libev
-mv $TARGET_DIR/include/*.h $TARGET_DIR/include/libev
+# fi
+# mkdir -p $TARGET_DIR/include/libev
+# mv $TARGET_DIR/include/*.h $TARGET_DIR/include/libev
 
-echo "*** Building mozjs ***"
-cd $BUILD_DIR/mozjs17*
-cd js/src
-# ./configure --disable-shared-js --prefix=$DESTDIR
-./configure --prefix=$TARGET_DIR
-# ./configure
-make
-# make install DESTDIR=$TARGET_DIR/mozjs_tmp
-make install
-# rsync -avI $TARGET_DIR/mozjs_tmp/usr/local/* $TARGET_DIR/ #--remove-source-files
-# update pc file
-MOZJS=$TARGET_DIR/lib/pkgconfig/mozjs-17.0.pc
-sed -e "s|prefix=|prefix=$TARGET_DIR|"  $MOZJS > mozjs_tmp.pc
-mv mozjs_tmp.pc $MOZJS
-sed -e "s|libdir=/lib|libdir=\${prefix}/lib|"  $MOZJS > mozjs_tmp.pc
-mv mozjs_tmp.pc $MOZJS
-sed -e "s|includedir=/include|includedir=\${prefix}/include|"  $MOZJS > mozjs_tmp.pc
-mv mozjs_tmp.pc $MOZJS
-rm -rf $TARGET_DIR/mozjs_tmp
+# echo "*** Building mozjs ***"
+# cd $BUILD_DIR/mozjs17*
+# cd js/src
+# # ./configure --disable-shared-js --prefix=$DESTDIR
+# ./configure --prefix=$TARGET_DIR
+# # ./configure
+# make
+# # make install DESTDIR=$TARGET_DIR/mozjs_tmp
+# make install
+# # rsync -avI $TARGET_DIR/mozjs_tmp/usr/local/* $TARGET_DIR/ #--remove-source-files
+# # update pc file
+# MOZJS=$TARGET_DIR/lib/pkgconfig/mozjs-17.0.pc
+# sed -e "s|prefix=|prefix=$TARGET_DIR|"  $MOZJS > mozjs_tmp.pc
+# mv mozjs_tmp.pc $MOZJS
+# sed -e "s|libdir=/lib|libdir=\${prefix}/lib|"  $MOZJS > mozjs_tmp.pc
+# mv mozjs_tmp.pc $MOZJS
+# sed -e "s|includedir=/include|includedir=\${prefix}/include|"  $MOZJS > mozjs_tmp.pc
+# mv mozjs_tmp.pc $MOZJS
+# rm -rf $TARGET_DIR/mozjs_tmp
 
 
 echo "*** Building pidgin ***"
 cd $BUILD_DIR/pidgin-*
-./configure --disable-screensaver --disable-gtkspell --disable-vv --disable-meanwhile --disable-avahi --disable-nm --with-static-prpls="gg irc jabber msn myspace mxit novell oscar simple yahoo zephyr" --enable-static --prefix=$TARGET_DIR 
-make -j $jval
-
+make distclean > /dev/null 2>&1 || true
+# ./configure --disable-screensaver --disable-gtkspell --disable-vv --disable-meanwhile --disable-avahi --disable-nm --with-static-prpls="gg irc jabber msn myspace mxit novell oscar simple yahoo zephyr" --enable-static --prefix=$TARGET_DIR 
+./configure # --prefix=$TARGET_DIR 
+# make -j $jval
+make
 # Don't fail when there are no warnings for gconf during the install
 # https://pidgin.im/pipermail/commits/2014-February/024518.html
-sed -i.bak '/grep -v "^Attached schema" 1>&2/s/$/ || true /' libpurple/gconf/Makefile
-make install
+#sed -i.bak '/grep -v "^Attached schema" 1>&2/s/$/ || true /' libpurple/gconf/Makefile
+make install #DESTDIR=/home/oglop/apps/pidgin
+
 
 
 echo "*** Building lwqq ***"
 cd $BUILD_DIR/lwqq
 git checkout -b dev origin/dev
-mkdir build && cd build
+rm -rf build > /dev/null 2>&1 || true
+mkdir build && cd build 
 
 sed -e "s|/usr/local/include/libev|/usr/local/include/libev\n$TARGET_DIR/include/libev|" $BUILD_DIR/lwqq/cmake/FindEV.cmake > FindEV_tmp.pc
 mv FindEV_tmp.pc $BUILD_DIR/lwqq/cmake/FindEV.cmake
 sed -e "s|/usr/local/lib64|/usr/local/lib64 $TARGET_DIR/lib|" $BUILD_DIR/lwqq/cmake/FindEV.cmake > FindEV_tmp.pc
 mv FindEV_tmp.pc $BUILD_DIR/lwqq/cmake/FindEV.cmake
 
-cmake ..
+cmake .. #-DCMAKE_INSTALL_PREFIX=$TARGET_DIR
 make
-make install DESTDIR=$TARGET_DIR/lwqq_tmp
-rsync -av $TARGET_DIR/lwqq_tmp/usr/local/* $TARGET_DIR/ --remove-source-files
+make install # DESTDIR=$TARGET_DIR/lwqq_tmp
+# rsync -av $TARGET_DIR/lwqq_tmp/usr/local/* $TARGET_DIR/ --remove-source-files
 sed -e "s|prefix=/usr/local|prefix=$TARGET_DIR|" $TARGET_DIR/lib/pkgconfig/lwqq.pc > xxx.pc
 mv xxx.pc $TARGET_DIR/lib/pkgconfig/lwqq.pc
-rm -rf TARGET_DIR/lwqq_tmp
+# rm -rf TARGET_DIR/lwqq_tmp
 
 
 echo "*** Building pidgin-lwqq ***"
 cd $BUILD_DIR/pidgin-lwqq
 git checkout -b dev origin/dev
+rm -rf build
 mkdir build && cd build
 cmake ..
 make
-make install DESTDIR=$TARGET_DIR/pidgin_tmp
-rsync -avI $TARGET_DIR/pidgin_tmp/usr/local/* $TARGET_DIR/ --remove-source-files
-rsync -avI $TARGET_DIR/pidgin_tmp/usr/share $TARGET_DIR/ --remove-source-files
-rsync -avI $TARGET_DIR/pidgin_tmp/$TARGET_DIR/* $TARGET_DIR/ --remove-source-files
-rm -rf TARGET_DIR/pidgin_tmp
+make install #DESTDIR=$TARGET_DIR/pidgin_tmp
+# rsync -avI $TARGET_DIR/pidgin_tmp/usr/local/* $TARGET_DIR/ --remove-source-files
+# rsync -avI $TARGET_DIR/pidgin_tmp/usr/share $TARGET_DIR/ --remove-source-files
+# rsync -avI $TARGET_DIR/pidgin_tmp/$TARGET_DIR/* $TARGET_DIR/ --remove-source-files
+# rm -rf TARGET_DIR/pidgin_tmp
 
 echo "*** Building pidgin-sendscreenshot ***"
 cd $BUILD_DIR/pidgin-sendscreenshot
 git fetch
 git checkout dev
-./configure --prefix=$TARGET_DIR
+# 
+sed -i.bak '/\#include <curl\/types.h>/ s/^#*/\/\//' /home/oglop/Downloads/pidgin-static/build/pidgin-sendscreenshot/src/main.h
+./configure #--prefix=$TARGET_DIR
 make
 make install
 
@@ -167,7 +180,7 @@ make install
 
 echo "*** Building Recent-Contacts-Plugin-for-Pidgin ***"
 cd $BUILD_DIR/Recent-Contacts-Plugin-for-Pidgin*
-cmake -D CMAKE_INSTALL_PREFIX=$TARGET_DIR .
+cmake . #-DCMAKE_INSTALL_PREFIX=$TARGET_DIR .
 make
 make install
 
@@ -179,14 +192,14 @@ wget http://ftp.gnome.org/pub/GNOME/sources/libnotify/0.7/libnotify-0.7.6.tar.xz
 tar -xvf libnotify-0.7.6.tar.xz
 rm libnotify-0.7.6.tar.xz
 cd $BUILD_DIR/libnotify*
-./configure --prefix=$TARGET_DIR
+./configure #--prefix=$TARGET_DIR
 make
 make install
 
 echo "*** Building pidgin-libnotify ***"
 cd $BUILD_DIR/pidgin-libnotify*
 ./autogen.sh
-./configure --prefix=$TARGET_DIR
+./configure #--prefix=$TARGET_DIR
 make
 make install
 
